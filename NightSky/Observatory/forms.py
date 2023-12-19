f4a0e9be9fe263f5ce69fb7a42253326195d183b
Anikanna44 <125280382+Anikanna44@users.noreply.github.com>
2023-12-19 18:50:17 +0100

    import data in database manually

--- NightSky/Observatory/forms.py
+++ NightSky/Observatory/forms.py
@@ -2,5 +2,8 @@ from django import forms
 
 
 class DirectoryForm(forms.Form):
-    directory_path = forms.CharField(widget=forms.TextInput(attrs={'class': 'input-class', 'placeholder': 'File path'}),
+    directory_path = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Directory path',
+                                                                   'id': 'directory_path',
+                                                                   'name': 'directory_path',
+                                                                   'type': 'text'}),
                                      required=True)
