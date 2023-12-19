0f4b818e6c8e44d78e79f6e8b3d40b8c8e5eda8e
Ema Adamova <adamova47@uniba.sk>
2023-12-19 17:29:45 +0100

    Observatory: add url pattern for directory_path_view

--- NightSky/Observatory/urls.py
+++ NightSky/Observatory/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, import_fits, export_fits
+from .views import home, import_fits, export_fits, directory_path_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('import_fits/', import_fits, name='import_fits'),
     path('export_fits/', export_fits, name='export_fits'),
+    path('import_fits/', directory_path_view, name='import_fits')
 ]
