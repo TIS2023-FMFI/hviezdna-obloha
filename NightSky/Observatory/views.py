204fd8d7a84755b1d7719b428c84738f9b6d4a03
Anikanna44 <125280382+Anikanna44@users.noreply.github.com>
2024-01-29 21:04:08 +0100

    add fisrt insert and generating log

--- NightSky/Observatory/views.py
+++ NightSky/Observatory/views.py
@@ -1,7 +1,9 @@
 from .forms import DirectoryForm
 import django.db.utils
 from django.shortcuts import render
-from .scripts.parsing import Parsing
+from .scripts.first_insert import process_folders_with_fits
+from .scripts.insert import Insert
+from .scripts.create_log import Log
 from django.db import connection
 
 
@@ -16,8 +18,17 @@ def import_fits(request):
         if form.is_valid():
             directory_path = form.cleaned_data['directory_path']
             directory_path = 'D:/' + directory_path[15:] #change to your path to fits images instead of 'D:/'
-            p = Parsing(directory_path)
-            result = execute_query(str(p))
+
+            first_insert = False
+            if first_insert:
+                process_folders_with_fits(directory_path)
+            else:
+                # daily insert with generating log
+                insert = Insert(directory_path)
+                del insert
+                log = Log(directory_path)
+                log.generate_log()
+
     else:
         form = DirectoryForm()
     return render(request, 'Observatory/import_fits.html', {'form': form, 'result': result})
@@ -26,15 +37,3 @@ def import_fits(request):
 def export_fits(request):
     return render(request, 'Observatory/export_fits.html')
 
-
-def execute_query(query):
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute(query)
-            return 'DONE'
-        except django.db.utils.IntegrityError:
-            return 'Already in database'
-
-
-def export_fits(request):
-    return render(request, 'Observatory/export_fits.html')
