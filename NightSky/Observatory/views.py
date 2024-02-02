53075eb3c5888d36d0024c7a958e75c1d98a6846
Ema Adamova <adamova47@uniba.sk>
2024-02-02 22:50:12 +0100

    Observatory: add success/failure messages

--- NightSky/Observatory/views.py
+++ NightSky/Observatory/views.py
@@ -11,7 +11,7 @@ from .scripts.generate_sky_map import generate_sky_map
 import tkinter as tk
 from tkinter import filedialog
 import os
-
+from django.contrib import messages
 from datetime import datetime, timedelta
 
 
@@ -53,10 +53,24 @@ def import_fits(request):
     if request.method == "POST":
         if 'import_last_night' in request.POST and last_added_directory_path:
             directory_path = last_added_directory_path
-            process_and_log_directory(directory_path, request)
-        elif form.is_valid():
-            directory_path = form.cleaned_data["directory_path"]
-            process_and_log_directory(directory_path, request)
+            success, message = process_and_log_directory(directory_path, request)
+            if success:
+                messages.success(request, message)
+            else:
+                messages.error(request, f'Import failed: {message}')
+            request.session['form_submitted'] = 'import_last_night'
+
+        elif 'import_directory' in request.POST:
+            if form.is_valid():
+                directory_path = form.cleaned_data["directory_path"]
+                success, message = process_and_log_directory(directory_path, request)
+                if success:
+                    messages.success(request, message)
+                else:
+                    messages.error(request, f'Import failed: {message}')
+            else:
+                messages.error(request, 'Incorrect input: Please ensure the directory path is correct.')
+            request.session['form_submitted'] = 'import_directory'
 
     return render(request, "Observatory/import_fits.html", {
         "form": form,
@@ -65,15 +79,19 @@ def import_fits(request):
 
 
 def process_and_log_directory(directory_path, request):
-    first_insert = False
-    if first_insert:
-        process_folders_with_fits(directory_path)
-    else:
-        insert = Insert(directory_path)
-        del insert
-        log = Log(directory_path)
-        log.generate_log()
-    generate_sky_map()
+    try:
+        first_insert = False
+        if first_insert:
+            process_folders_with_fits(directory_path)
+        else:
+            insert = Insert(directory_path)
+            del insert
+            log = Log(directory_path)
+            log.generate_log()
+        generate_sky_map()
+        return True, "Import successful."
+    except Exception as e:
+        return False, str(e)
 
 
 def export_fits(request):  # TODO: REMOVE PRINTS
@@ -107,28 +125,28 @@ def number_of_nights(request):
             adjusted_dates.add(date_time.date())
 
         return len(adjusted_dates)
-    return 0
+    return None
 
 
 def number_of_frames(request):
     if FitsImage.objects.exists():
         frames = FitsImage.objects.count()
         return frames
-    return 0
+    return None
 
 
 def last_light_frames_night(request):
     if FitsImage.objects.filter(IMAGETYP="LIGHT").exists():
         light_frames = FitsImage.objects.filter(IMAGETYP="LIGHT").latest("DATE_OBS").DATE_OBS
         return light_frames
-    return 0
+    return None
 
 
 def last_calib_frames_night(request):
     if FitsImage.objects.filter(IMAGETYP="CALIB").exists():
         calib_frames = FitsImage.objects.filter(IMAGETYP="CALIB").latest("DATE_OBS").DATE_OBS
         return calib_frames
-    return 0
+    return None
 
 
 def last_ccd_temperature(request):
@@ -136,4 +154,4 @@ def last_ccd_temperature(request):
         last_fits_image = FitsImage.objects.latest("DATE_OBS")
         ccd_temp = last_fits_image.CCD_TEMP
         return ccd_temp
-    return 0
+    return None
